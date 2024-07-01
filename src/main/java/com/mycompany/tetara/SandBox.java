5bce2b21671879bbcf50cd68603e662c5608fa25
Qyuzet <riqyuzet@gmail.com>
2024-07-01 23:27:44 +0700

    V0.0.7

--- src/main/java/com/mycompany/tetara/SandBox.java
+++ src/main/java/com/mycompany/tetara/SandBox.java
@@ -452,7 +452,7 @@ public class SandBox {
         validatorStakes.put("Validator7", 2000);
         validatorStakes.put("Validator8", 2500);
         validatorStakes.put("Validator9", 1500);
-        validatorStakes.put("Validator10", 300);
+        validatorStakes.put("Validator10", 200);
 
         // Add PPE nodes
         validatorStakes.put("ppe_Node1", 100); // Example PPE node stakes
